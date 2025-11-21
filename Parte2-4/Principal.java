25aa45565ca58f1a8c7d323a3fbd3be0e9f4fae6
Juan Silva <juan.silva96@hotmail.com>
2025-11-21 18:26:24 -0300

    Implement age validation in Principal class

---
+++ Parte2-4/Principal.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Nilus
+ */
+public class Principal {
+    public static void main(String[] args) {
+        int edad = -5; // Cambiar para probar
+
+        try {
+            validarEdad(edad);
+            System.out.println("Edad válida: " + edad);
+        } catch (EdadInvalidaException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void validarEdad(int edad) throws EdadInvalidaException {
+        if (edad < 0 || edad > 120) {
+            throw new EdadInvalidaException("La edad debe estar entre 0 y 120.");
+        }
+    }
+}
