20487f4cfc73d934f5ccee0b9d720566540758aa
Juan Silva <juan.silva96@hotmail.com>
2025-11-21 18:18:26 -0300

    Create Notificable interface for state notification

---
+++ Parte 1/Notificable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Nilus
+ */
+
+public interface Notificable {
+    void notificarCambioEstado(String estado);
+}
